4e7a775311e2fa293ede8e2ae626343b620404f9
Ali Zaib <alizaib2706@gmail.com>
2021-03-06 14:33:23 +0500

    chat-inProgress

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import "../css/Chat.css" ;
+import React , {Component} from 'react' ;
+import io from "socket.io-client" ;
+import events from "../events" ;
+import SampleCheck from './chatData/SampleCheck' ;
+
+//  import "../css/Chat.css" ;
+import ChatSidebar from "./chatData/ChatSidebar" ;
 function Chat() {
     return (
         <div className="chat" >
-            This is the chat component
+           {/*  <div className="chat__sidebar">
+                <ChatSidebar />
+            </div>
+            <div className="chat__content">
+                This is the chat content area
+            </div> */}
+            <SampleCheck />
         </div>
     )
 }
 
 export default Chat
+
+
---
+++ src/components/chatData/ChatContact.js
@@ -0,0 +1,12 @@
+import { Avatar } from '@material-ui/core'
+import React from 'react'
+
+function ChatContact() {
+    return (
+        <div className="chatContact" >
+            <Avatar />
+        </div>
+    )
+}
+
+export default ChatContact
