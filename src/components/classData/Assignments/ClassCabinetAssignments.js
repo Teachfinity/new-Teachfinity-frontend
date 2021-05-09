62caec6de8f59273ec708891ab58962007d422ca
MAliZaib <alizaib.alphasquad@gmail.com>
2021-05-09 22:31:10 +0500

    Assignments screen in progress

---
+++ src/components/classData/Assignments/ClassCabinetAssignments.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../../../css/ClassCabinetAssignments.css"
+function ClassCabinetAssignments() {
+    return (
+        <div class="cabinetAssignments" >
+            This is the Assignments screen
+        </div>
+    )
+}
+
+export default ClassCabinetAssignments
