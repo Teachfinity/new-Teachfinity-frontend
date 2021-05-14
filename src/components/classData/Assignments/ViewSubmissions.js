66e534bd51271a1c46809c0b29749d9d29cd8db7
Mahno-or <mahnoorsaleem.comsats@gmail.com>
2021-05-14 14:44:31 +0500

    viewAssignment Backend

--- src/components/classData/Assignments/ViewSubmissions.js
+++ src/components/classData/Assignments/ViewSubmissions.js
@@ -1,8 +1,53 @@
 import React, {useEffect, useState} from 'react';
 import "../../../css/viewSubmissions.css" ;
+import {useSelector , useDispatch} from "react-redux" ;
+import {selectedClass} from "../../../features/selectClassSlice" ;
+import { selectedAssignment } from "../../../features/selectedAssignmentSlice";
+import axios from "axios" ;
+
 function ViewSubmissions() {
+
+    const [isBusy , setBusy] = useState(true) ;
+    const [sname , setSname] = useState([{user:"", file:"", fileUrl:"", submission:""}]) ;
+    const [pending , setPending] = useState([]) ;
+    const dispatch = useDispatch();
+    const selectClass = useSelector(selectedClass) ;
+    const selectAssignment = useSelector(selectedAssignment) ;
+
+    useEffect(() => {
+        /* response for the assignments */
+        setSname([])
+        setPending([])
+        axios.get("http://localhost:5000/classes/getstudents/class/"+selectClass.id)
+        .then((res)=>{
+             res.data.map((id)=>{
+                 console.log(id.sid)
+                var userid = id.sid._id
+                var username = id.sid.name
+                axios.get("http://localhost:5000/assignments/getassignments/"+selectAssignment.aid)
+                .then((res)=>{
+                    if(res.data[0].studentfiles[0].sid===userid){
+                        setSname(sname=> [...sname, {user: username, file: res.data[0].studentfiles[0].fileNme, 
+                        fileUrl: res.data[0].studentfiles[0].fileUrl, submission: res.data[0].studentfiles[0].submittedAt}])
+                        console.log("here")
+                    }
+                    else{
+                        setPending(pending => [...pending, username])
+                    }
+                })
+                .catch(err => alert("MY FEED SAYs" + err))  
+            }) 
+        })
+        .catch(err => alert("MY FEED SAYs" + err))  
+        .then(()=>{
+            setTimeout(() => {
+                setBusy(false) ;
+              }, 2000);
+        })
+        .catch(err => alert("MY FEED SAYs" + err))  
         
-    
+    } , [])
+
     return (
         <div className="viewSubmissions" >
             <div className="viewSubmissions__heading">
@@ -15,25 +60,26 @@ function ViewSubmissions() {
                     Submissions
                 </h1>
                 {/*map student submission in a Component for showing shubmission */}
+                <ol>
+                {sname.map((item, index)=>(
+                    <li>{item.name}</li>
+                ))}
+                </ol>
             </div>
             <div className="viewSubmission__notSubmitted">
                 <h1>
                     Pending submissions
                 </h1>
-                <ol>
                     {/* Map the name in this list */}
-                    <li>Muhammad Ali Zaib</li>
-                    <li>Muhammad Ali Zaib</li>
-                    <li>Muhammad Ali Zaib</li>
-                    <li>Muhammad Ali Zaib</li>
-                    <li>Muhammad Ali Zaib</li>
-                    <li>Muhammad Ali Zaib</li>
-                    <li>Muhammad Ali Zaib</li>
-                </ol>
+                    <ol>
+                    {pending.map((item, index)=>(
+                        <li>{item}</li>
+                    ))}
+                    </ol>
             </div>
             <div className="viewAssignment__actionButtons" >
                 <button className="viewAssignment__downloadButton" >Download All</button>
-                <button className="viewAssignment__downloadPlagiarism">Download Plagiarism report</button>
+                <button className="viewAssignment__downloadPlagiarism">Download Plagiarism Report</button>
             </div>
         </div>
     )
