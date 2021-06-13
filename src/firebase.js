1df8897f412c85b7b763cc776f2511967b29435f
MAliZaib <alizaib.alphasquad@gmail.com>
2021-06-13 15:56:35 +0500

    latest update

--- src/firebase.js
+++ src/firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   const firebaseApp = firebase.initializeApp(firebaseConfig) ;
 
   const db = firebaseApp.firestore() ;
- const storageRef = firebaseApp.storage().ref()
+  const storageRef = firebaseApp.storage().ref() ;
   const auth = firebase.auth() ;
   const provider = new firebase.auth.GoogleAuthProvider() ;
 
